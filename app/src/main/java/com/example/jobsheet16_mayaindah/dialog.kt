d45d7ced2dfc69f755cbe02637040e4388ff5a76
NorHidayah <salma@gmail.com>
2022-10-19 07:07:15 +0700

    first commit

---
+++ app/src/main/java/com/example/jobsheet16_mayaindah/dialog.kt
@@ -0,0 +1,11 @@
+package com.example.jobsheet16_mayaindah
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class dialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialog)
+    }
+}
